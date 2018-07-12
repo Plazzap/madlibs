55240cf05a3cf3c7afbc554ff2985a723f1e780a
Carter Harris <carter.kc.harris@gmail.com>
2018-07-12 14:38:32 +0000

    Built first functional component

---
+++ src/components/header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div className="header">
+       <h1>Bottega Madlibs</h1>
+            <h1>{title}</h1>
+            <p>{content}</p>
+        </div>
+    
+    )
+}
+
+export default Header;
